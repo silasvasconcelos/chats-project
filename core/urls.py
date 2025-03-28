5e38f1fc2009c50b52dc5991093602dcd5905f36
Silas Vasconcelos <silasvasconcelos@hotmail.com.br>
2025-03-28 14:49:14 -0300

    feat: added chats, files and messages endpoints

--- core/urls.py
+++ core/urls.py
@@ -14,14 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # Optional UI:
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    path("", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/v1/", include("applications.chats.urls")),
 ]
